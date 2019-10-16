8901477485a117cb6bb187a118952e45fbc3b043
Bearnies <44014883+Bearnies@users.noreply.github.com>
2019-10-16 20:32:30 +0700

    Migrated db into auth, ignored db folder

--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,40 @@ const express = require('express');
 const router  = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+var mysql = require('mysql');
+
+var createConnection =  mysql.connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'bearnies',
+        password: 'khanghi1',
+        database: 'btcn06web'
+});
+
+createConnection.connect(function(error) {
+    if (error) throw error;
+    console.log("Connected to database!");
+});
+
+
+const addUser = async ({ name, password }) => {
+    return await User.create({ name, password });
+};
 
 //POST register
 router.post('/register', function (req, res, next) {
-    let newUser = new User({
-        username: req.body.username,
-        password: req.body.password
+    const {username, password} = req.body;
+    let newUser = new User ({
+        username: username,
+        password: password
     });
-
-    User.addUser(newUser, (err, user) => {
-        if (err) {
-            res.json({ success: false, msg: 'Failed to register user!' });
+    
+    //Thêm người dùng
+    addUser(newUser, (error, user) => {
+        if (error) {
+            res.json({ success: false, msg: 'Failed to register user' });
         } else {
-            res.json({ success: true, msg: 'User registered' });
+            res.json({ success: true, user, msg: 'User registered' });
         }
     });
 });
@@ -22,19 +43,20 @@ router.post('/register', function (req, res, next) {
 
 //POST login
 router.post('/login', function (req, res, next) {
-    passport.authenticate('local', {session: false}, (err, user, info) => {
-        if (err || !user) {
+    passport.authenticate('local', {session: false}, (error, user, info) => {
+        if (error || !user) {
             return res.status(400).json({
                 message: 'Something is not right',
                 user: user
             });
         }
-       req.login(user, {session: false}, (err) => {
-           if (err) {
-               res.send(err);
+        
+       req.login(user, {session: false}, (error) => {
+           if (error) {
+               res.send(error);
            }
 
-           //Tạo web token cho nội dung đối tượng người dùng và trả về
+           //Tạo web token cho nội dung đối tượng người dùng và trả về jwt
            const token = jwt.sign(user, 'your_jwt_secret');
            return res.json({user, token});
         });
